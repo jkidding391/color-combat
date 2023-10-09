d8bfc11543a0ee3076533870f2225b5c28427fe6
Jonatan Salmeron <jonajere1@hotmail.com>
2023-10-09 16:21:12 -0400

    EnemyDummy now look at player and can be destroyed

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] Transform target;
+
     void Start()
     {
         
@@ -13,13 +15,21 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleRotation();
         
     }
-    void OnCollisionEnter(Collision other) {
-        if (other.gameObject.tag == "Bullet"){
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag.Equals("Bullet")){
             Destroy(gameObject);
             
         }//if
 
+    }
+
+    private void HandleRotation() {
+        if (target != null) {
+            transform.LookAt(target);
+
+        }
     }
 }
